45689984f88c74a6f1bc763c1fad8798ec4d1b78
[·Æï]-ÌÆС¶« <13594525237@139.com>
2019-10-16 17:55:41 +0800

    add mqtt test

---
+++ MqttTest/MqttClientWin/FmMqttClient.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace MqttClientWin
+{
+    partial class FmMqttClient
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSubTopic = new System.Windows.Forms.TextBox();
+            this.txtReceiveMessage = new System.Windows.Forms.TextBox();
+            this.txtPubTopic = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSendMessage = new System.Windows.Forms.TextBox();
+            this.BtnPublish = new System.Windows.Forms.Button();
+            this.btnSubscribe = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "订阅主题";
+            // 
+            // txtSubTopic
+            // 
+            this.txtSubTopic.Location = new System.Drawing.Point(92, 47);
+            this.txtSubTopic.Name = "txtSubTopic";
+            this.txtSubTopic.Size = new System.Drawing.Size(297, 21);
+            this.txtSubTopic.TabIndex = 1;
+            this.txtSubTopic.Text = "position";
+            // 
+            // txtReceiveMessage
+            // 
+            this.txtReceiveMessage.Location = new System.Drawing.Point(35, 92);
+            this.txtReceiveMessage.Multiline = true;
+            this.txtReceiveMessage.Name = "txtReceiveMessage";
+            this.txtReceiveMessage.Size = new System.Drawing.Size(425, 180);
+            this.txtReceiveMessage.TabIndex = 2;
+            // 
+            // txtPubTopic
+            // 
+            this.txtPubTopic.Location = new System.Drawing.Point(92, 291);
+            this.txtPubTopic.Name = "txtPubTopic";
+            this.txtPubTopic.Size = new System.Drawing.Size(368, 21);
+            this.txtPubTopic.TabIndex = 4;
+            this.txtPubTopic.Text = "manipulated";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "发布主题";
+            // 
+            // txtSendMessage
+            // 
+            this.txtSendMessage.Location = new System.Drawing.Point(35, 331);
+            this.txtSendMessage.Multiline = true;
+            this.txtSendMessage.Name = "txtSendMessage";
+            this.txtSendMessage.Size = new System.Drawing.Size(425, 77);
+            this.txtSendMessage.TabIndex = 5;
+            // 
+            // BtnPublish
+            // 
+            this.BtnPublish.Location = new System.Drawing.Point(182, 414);
+            this.BtnPublish.Name = "BtnPublish";
+            this.BtnPublish.Size = new System.Drawing.Size(95, 36);
+            this.BtnPublish.TabIndex = 6;
+            this.BtnPublish.Text = "发布";
+            this.BtnPublish.UseVisualStyleBackColor = true;
+            this.BtnPublish.Click += new System.EventHandler(this.BtnPublish_Click);
+            // 
+            // btnSubscribe
+            // 
+            this.btnSubscribe.Location = new System.Drawing.Point(411, 44);
+            this.btnSubscribe.Name = "btnSubscribe";
+            this.btnSubscribe.Size = new System.Drawing.Size(60, 23);
+            this.btnSubscribe.TabIndex = 7;
+            this.btnSubscribe.Text = "订阅";
+            this.btnSubscribe.UseVisualStyleBackColor = true;
+            this.btnSubscribe.Click += new System.EventHandler(this.BtnSubscribe_ClickAsync);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 414);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "断开";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 421);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "连接";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FmMqttClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 469);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSubscribe);
+            this.Controls.Add(this.BtnPublish);
+            this.Controls.Add(this.txtSendMessage);
+            this.Controls.Add(this.txtPubTopic);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtReceiveMessage);
+            this.Controls.Add(this.txtSubTopic);
+            this.Controls.Add(this.label1);
+            this.Name = "FmMqttClient";
+            this.Text = "MQTT订阅及发布客户端";
+            this.Load += new System.EventHandler(this.FmMqttClient_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSubTopic;
+        private System.Windows.Forms.TextBox txtReceiveMessage;
+        private System.Windows.Forms.TextBox txtPubTopic;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSendMessage;
+        private System.Windows.Forms.Button BtnPublish;
+        private System.Windows.Forms.Button btnSubscribe;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
